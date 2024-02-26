bcc42f81d3cebd90d5336f61777a56971a868819
Veez Remsik <almighty@veez.us>
2024-02-26 09:53:22 -0600

    Add jobs

---
+++ db/migrate/20240226154723_create_jobs.rb
@@ -0,0 +1,11 @@
+class CreateJobs < ActiveRecord::Migration[7.1]
+  def change
+    create_table :jobs do |t|
+      t.string :name
+      t.string :url
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
